95b9c5188cb5b3414d93f2a7bc27ef7ae90f67e5
Mike Harris <mikeharris22@gmail.com>
2023-04-18 23:54:23 -0400

    Added required language sets to Treesitter.

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+     ensure_installed = { "lua", "go", "typescript", "html", "css", "rust", "sql", "java", "fish", "javascript", "json", "ruby" },
   },
 }
