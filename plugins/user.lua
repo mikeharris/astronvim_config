80fb7adb97755ea265663e077b5e63ac1961840d
Mike Harris <mikeharris22@gmail.com>
2023-04-19 19:52:56 -0400

    Adding so go specific config and libs.

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,7 @@
+-- TODO opts is the same as calling config.setup()
+-- HACK this is a hack test
+-- FIX fix this soon
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +13,52 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "uloco/bluloco.nvim",
+    lazy = false,
+    priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
+    opts = {},
+  },
+  {
+    "rmehri01/onenord.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    -- lazy = false,
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    event = "User AstroFile",
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function() require("go").setup() end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }
